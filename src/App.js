f377c272018baac265db72ac50283aadb223bcb3
Lizzie Hard <elizabeth_hard@hotmail.co.uk>
2017-10-29 18:15:31 +0000

    updated render function

--- src/App.js
+++ src/App.js
@@ -7,26 +7,23 @@ class App extends Component {
   constructor(props) {
     // this.props
     super(props);
-
     this.state = {
       cryptos:[]
-    };
+    }
   }
 
   componentDidMount() {
     axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,IOT&tsyms=USD')
     .then(res => {
-      const cryptos = res.data;
-      console.log(cryptos);
-      // rerendered with updated state
       this.setState({cryptos: res.data});
     })
   }
 
   render() {
+    const {cryptos} = this.state;
     return (
       <div className="App">
-        {Object.keys(this.state.cryptos).map((key) => (
+        {Object.keys(cryptos).map((key) => (
           <div id={"crypto-container"} key={key}>
             <span className="left">{key}</span>
             <span className="right">{this.state.cryptos[key].USD}</span>
