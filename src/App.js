6515d0153e8faa0fa06e1f296c50951378b374bf
Lizzie Hard <elizabeth_hard@hotmail.co.uk>
2017-10-29 17:27:42 +0000

    added unique keys to array mapping

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    // this.props
+    super(props);
+
+    this.state = {
+      cryptos:[]
+    };
+  }
+
+  componentDidMount() {
+    axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,IOT&tsyms=USD')
+    .then(res => {
+      const cryptos = res.data;
+      console.log(cryptos);
+      // rerendered with updated state
+      this.setState({cryptos: res.data});
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {Object.keys(this.state.cryptos).map((key) => (
+          <div id={"crypto-container"} key={key}>
+            <span className="left">{key}</span>
+            <span className="right">{this.state.cryptos[key].USD}</span>
+          </div>
+        ))}
       </div>
-    );
+    )
   }
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,29 @@
 import raf from './tempPolyfills'
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { componentDidMount } from './App'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+
+
+it('returns data when get request called', (done) => {
+  let mock = new MockAdapter(axios);
+  mock.onGet('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,IOT&tsyms=USD').reply(200,{
+    data: {
+      posts: ['intro to git']
+    }
+  });
+
+  let response = componentDidMount();
+
+  expect(response.data[0]).to.be.equal(USD)
+
+
 });
