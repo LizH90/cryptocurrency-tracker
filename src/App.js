30b734cbe3b17166e5900367343e7c437434c074
Lizzie Hard <elizabeth_hard@hotmail.co.uk>
2017-10-31 09:39:13 +0000

    bitcoin historic price chart added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
+import {Line} from 'react-chartjs-2';
 import axios from 'axios';
 import './App.css';
-import Chart from './components/Chart.js'
+// import Chart from './components/Chart.js'
 var NumberFormat = require('react-number-format');
+
 
 class App extends Component {
 
   constructor(props) {
-    // this.props
     super(props);
     this.state = {
       cryptos:[],
-      chartData:{}
+      chartData: {
+        labels: []
+      }
     }
   }
 
@@ -20,44 +23,73 @@ class App extends Component {
   }
 
   getChartData(){
-    //Axios call here
-    this.setState({
-      chartData:{
-        labels: ['BTC', 'ETH', 'Something'],
-        datasets:[
-          {label: 'Price in £',
-            data: [
-              1234,
-              2436,
-              5734
-            ],
-            backgroundColor:['darkblue']
-          }
-        ]
-      }
+    axios.get('https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=USD&limit=60&aggregate=3&e=CCCAGG')
+      .then(res => {
+        var bitcoin = res.data.Data
+        let date=[]
+        let value=[]
+        for (let i=0; i<bitcoin.length; i++){
+          date.push(bitcoin[i].time)
+          value.push(bitcoin[i].open)
+        }
+        console.log(date)
+        console.log(value)
+          this.setState({
+            chartData: {
+              labels: date,
+              datasets:[
+                { data: value
+                }
+              ]
+            }
+          });
+          console.log(this.state)
+        }
+    ).catch(function (error) {
+      console.log(error);
     })
+    console.log(this.state)
   }
-
-
 
   componentDidMount() {
     axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,IOT&tsyms=USD')
       .then(res => {
       this.setState({cryptos: res.data});
+    }).catch(function (error) {
+      console.log(error);
     })
   }
 
   render() {
-    const {cryptos} = this.state;
     return (
       <div className="App">
-        {Object.keys(cryptos).map((key) => (
+        {Object.keys(this.state.cryptos).map((key) => (
           <div id={"currency-container"} key={key}>
             <span className="currency">{key}</span>
             <span className="price"><NumberFormat value={this.state.cryptos[key].USD} displayType={'text'} decimalScale={2} fixedDecimalScale={true} thousandSeparator={true} prefix={'$'} /></span>
           </div>
         ))}
-        <Chart chartData={this.state.chartData}/>
+        <div className="chart">
+        <Line
+          data={this.state.chartData}
+          options={{
+            title:{
+              display:this.props.displayTitle,
+              text: 'Bitcoin to USD',
+              fontSize: 20
+            },
+            legend:{
+              display:this.props.displayLegend,
+              position:this.props.legendPosition,
+              labels:{
+                fontColor:'#000'
+              }
+            }
+          }}
+        />
+        </div>
+
+
       </div>
     )
   }
