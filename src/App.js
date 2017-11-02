52f0c9422702691ff350fa6672d800ab1071afd5
Lizzie Hard <elizabeth_hard@hotmail.co.uk>
2017-11-02 11:35:00 +0000

    updated page title

--- src/App.js
+++ src/App.js
@@ -97,7 +97,7 @@ class App extends Component {
   render() {
     return (
       <div className="Title">
-      <h1> Bitcoin Price Tracker USD </h1>
+      <h1> CryptoCurrency Price Tracker</h1>
       <br></br>
       <div className="App">
         {Object.keys(this.state.cryptos).map((key) => (
